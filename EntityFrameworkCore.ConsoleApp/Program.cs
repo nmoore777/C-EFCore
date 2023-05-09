adfce9d54599f16e58d7c7433c4bf89f8b8d4a0d
nmoore777 <nathanmoore777@gmail.com>
2023-05-09 10:49:39 -0500

    inserting one to many data

--- EntityFrameworkCore.ConsoleApp/Program.cs
+++ EntityFrameworkCore.ConsoleApp/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace EntityFrameworkCore.ConsoleAppv
+﻿using EntityFrameworkCore.Data;
+using EntityFrameworkCore.Domain;
+
+namespace EntityFrameworkCore.ConsoleAppv
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static FootballLeageDbContext context = new FootballLeageDbContext();
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var league = new League { Name = "Red Premiere League" };
+            
+           // await AddNewLeague(league);
+           // await AddTeamsWithLeagueId(league);
+
+
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
+        }
+
+        static async Task AddNewLeague(League league)
+        {
+            if(context.Leagues.)
+            await context.Leagues.AddAsync(league); //define db context - table to interact with - option (object) -- ac tion happens in memory
+            await context.SaveChangesAsync(); //generate sql and execute action on db
+        }
+
+        static async Task AddTeamsWithLeagueId(League league)
+        {
+            var teams = new List<Team> { 
+            new Team
+            {
+                Name = "A",
+                LeagueId = league.Id
+            },
+            new Team
+            {
+                Name = "B",
+                LeagueId = league.Id
+            },
+            new Team
+            {
+                Name = "C",
+                LeagueId = league.Id
+            }
+            };
+            await context.AddRangeAsync(teams);
+            await context.SaveChangesAsync(); //generate sql and execute action on db
         }
     }
 }
--- EntityFrameworkCore.Data/FootballLeageDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeageDbContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace EntityFrameworkCore.Data
 {
@@ -12,7 +13,9 @@ namespace EntityFrameworkCore.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt=False")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+                .EnableSensitiveDataLogging();
         } 
 
         public DbSet<Team> Teams { get; set; }
