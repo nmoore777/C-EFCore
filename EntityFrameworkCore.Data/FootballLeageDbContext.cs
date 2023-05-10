13bd4ad3a238f70a57385216a6b6e5c9a575c343
nmoore777 <nathanmoore777@gmail.com>
2023-05-10 11:02:41 -0500

    probably need to start a gitignore in the future

---
+++ EntityFrameworkCore.Data/FootballLeageDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkCore.Data
+{
+    public class FootballLeageDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt=False")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        } 
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+
+    }
+}
